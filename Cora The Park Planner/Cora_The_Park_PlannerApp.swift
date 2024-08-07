24f12daa092d2976c66dccc596bf852c713a827f
Isabela Ferreira <iacferreira88@icloud.com>
2024-08-07 09:59:17 -0400

    Initial Commit

---
+++ Cora The Park Planner/Cora_The_Park_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cora_The_Park_PlannerApp.swift
+//  Cora The Park Planner
+//
+//  Created by Isabela Ferreira on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Cora_The_Park_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
